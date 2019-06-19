b713531584ab47f70f95f9fbf8ef34487ffc5e2e
crossgear <anone.gim999@gmail.com>
2019-06-19 07:41:12 -0400

    comienzon

---
+++ contador/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:contador/src/app.dart';
+import 'package:flutter/material.dart';
+
+//mis importaciones
+//import 'src/app.dart';
+
+
+void main() {
+  runApp( MyApp() );
+}
---
+++ contador/lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:contador/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: HomePage(),
+      ),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+      ),
+    );
+  }
+}
