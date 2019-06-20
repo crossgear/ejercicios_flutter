d3cf9d1c73a11be7d9d3f126d10b0e96e67a5dc6
crossgear <anone.gim999@gmail.com>
2019-06-19 22:20:38 -0400

    cambio

--- contador/lib/src/pages/home_page.dart
+++ contador/lib/src/pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget{
         //centerTitle: true,
       ),
       body: Center(
-        child: Text("Hola mundo!",
+        child: Text("Numero de Clicks:",
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.w600
